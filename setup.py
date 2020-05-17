86e5d78efccefebd75c400f12780f324cb18d916
Ryan Swan <rmswan@gmail.com>
2020-05-16 17:42:35 -0700

    cookiecutter directory template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using the lyrics.com database to perform genre classification using unsupervised and supervised methods',
+    author='Ryan Swan',
+    license='',
+)
